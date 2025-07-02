b0de3036ef00c51f52cb7352ef3a5e16d4af251b
abhinav <abhinavgupta0148@gmail.com>
2025-07-02 01:24:08 -0400

    Add Sphinx documentation and Read the Docs config

---
+++ docs/conf.py
@@ -0,0 +1,18 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'Spliit Client'
+copyright = '2024, Abhinav'
+author = 'Abhinav'
+release = '0.1.0'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'sphinx_rtd_theme' 
---
+++ docs/index.rst
@@ -0,0 +1,13 @@
+Spliit Client Documentation
+==========================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+Welcome to the Spliit Client documentation!
+
+.. automodule:: spliit_client
+   :members:
+   :undoc-members:
+   :show-inheritance: 
